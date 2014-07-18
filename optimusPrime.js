69310f9d49cc1b894a911261df080f25bb346f9f
Joaquin Guardado <betwinsouls@gmail.com>
2014-07-18 11:18:01 -0700

    Version 01 added js file
    Currently optimusPrime needs input when calling the function.

---
+++ optimusPrime.js
@@ -0,0 +1,18 @@
+function optimusPrime(n) {
+    isPrime = true;
+    var isPrime = true;
+    for (var i = 2; i < n && isPrime; i++) {
+    if (n % i === 0) {
+        isPrime = false;     
+    } else {
+    isPrime = true;
+    }
+  } if(isPrime){
+    console.log("The migthy All Spark says is prime!");
+  }
+  else {
+    console.log("Trust the cube! this number is not a prime.");
+  }
+}
+ 
+optimusPrime(2013);
