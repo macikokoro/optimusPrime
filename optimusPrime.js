baa756ff576eeca97df66e177aeb10f2403c8dc1
Joaquin Guardado <betwinsouls@gmail.com>
2014-07-25 10:01:45 -0700

    Added prompt to ask for a number from the user.

--- optimusPrime.js
+++ optimusPrime.js
@@ -1,18 +1,21 @@
+
 function optimusPrime(n) {
     isPrime = true;
     var isPrime = true;
     for (var i = 2; i < n && isPrime; i++) {
-    if (n % i === 0) {
-        isPrime = false;     
-    } else {
-    isPrime = true;
+        if (n % i === 0) {
+            isPrime = false;
+        } else {
+            isPrime = true;
+        }
     }
-  } if(isPrime){
-    console.log("The migthy All Spark says is prime!");
-  }
-  else {
-    console.log("Trust the cube! this number is not a prime.");
-  }
+    if (isPrime) {
+        console.log("The migthy All Spark says is prime!");
+    } else {
+        console.log("Trust the cube! this number is not a prime.");
+    }
 }
- 
-optimusPrime(2013);
+
+var candidate = prompt("I Optimus Prime request a number!")
+
+optimusPrime(candidate);
